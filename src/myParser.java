358201c6a005416fc5d78a5d2df9249689719163
Seungmin Jeong <seungmin1266@gmail.com>
2022-05-05 19:44:15 +0900

    commit for init

---
+++ src/myParser.java
@@ -0,0 +1,11 @@
+import java.net.URL;
+
+public class myParser {
+    public static void main(String[] args) {
+        String hBl;
+        String year;
+        String adress = "https://unipass.customs.go.kr:38010/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?crkyCn=&hblNo=&blYy=2022";
+
+
+    }
+}
